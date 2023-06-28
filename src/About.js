cdb46c52142e7b6108e769941e32c5ae64b44e4e
Vincent Anjiri <vincentanjiri12@gmail.com>
2023-06-28 14:33:43 +0300

    first commit

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+      <main className='About'>
+          <h2>About</h2>
+          <p style={{ marginTop: "1rem" }}>This blog app is a project in my journey to Learn React.</p>
+      </main>
+  )
+}
+
+export default About
